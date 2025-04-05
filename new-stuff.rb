aea85575e6041c6234ebe6f70033b9b0cc5eba3c
PetitLuc38 <78622151+PetitLuc38@users.noreply.github.com>
2025-04-05 20:06:43 +0200

    Update new-stuff.rb

--- new-stuff.rb
+++ new-stuff.rb
@@ -2,10 +2,11 @@ class TgTimer < Formula
   desc "A program for timing mechanical watches"
   homepage "https://tg.ciovil.li"
   url "https://github.com/PetitLuc38/New-stuff/archive/refs/tags/v0.6.1-tpiepho.tar.gz"
-  sha256 "c88889bd391212d9104bca9918fb573a4ff96a752717b4a779d33f637c633ce7"
+  sha256 "a219eaeda2e6c97fb61ae93ae0af80eacaa1fb53fc83d39918ad35e55ff9bb91"
   head "https://github.com/PetitLuc38/New-stuff.git", :branch => "new-stuff"
 
   depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
